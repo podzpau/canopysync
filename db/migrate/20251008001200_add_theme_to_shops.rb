27fc06215c342489da28ea7a51cf187e8dfbce43
podzpau <paulius@canopysync.io>
2025-10-13 10:16:03 -0600

    Fix duplicate column migration

--- db/migrate/20251008001200_add_theme_to_shops.rb
+++ db/migrate/20251008001200_add_theme_to_shops.rb
@@ -1,7 +1,5 @@
 class AddThemeToShops < ActiveRecord::Migration[8.0]
   def change
-    add_column :shops, :primary_color, :string, default: '#10b981'
-    add_column :shops, :secondary_color, :string, default: '#3b82f6'
     add_column :shops, :font_family, :string, default: 'Inter'
     add_column :shops, :custom_font_url, :string
   end
